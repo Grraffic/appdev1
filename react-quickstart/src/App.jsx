c76923b358c65e0e276d5b2870cc22f84d1aff35
Rafael Ramos <ramosraf278@gmail.com>
2024-10-10 18:02:02 +0800

    I'm done the section for conditional rendering

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
-import Profile from "./profile";
+import AdminPanel from "./AdminPanel";
+import LoginForm from "./LoginForm";
+
 function App() {
+  // let content;
+  let isLoggedIn = true;
+
+  // if (isLoggedIn) {
+  //   content = <AdminPanel />;
+  // } else {
+  //   content = <LoginForm />;
+  // }
+
   return (
     <>
       <h1>Welcome to my app</h1>
-      <Profile />
+      {/* {isLoggedIn ? <AdminPanel /> : <LoginForm />} */}
+      {isLoggedIn && <AdminPanel />}
     </>
   );
 }
