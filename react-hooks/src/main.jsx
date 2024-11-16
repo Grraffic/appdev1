933bcb4d64fff82043f652a2be27172598a87396
Rafael Ramos <Ramosraf278@gmail.com>
2024-11-16 13:47:40 +0800

    Done TextInput

--- react-hooks/src/main.jsx
+++ react-hooks/src/main.jsx
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 import Counter from "./components/Counter.jsx";
 import ToggleButton from "./components/ToggleButton.jsx";
+import TextInput from "./components/TextInput.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ToggleButton />
+    <TextInput />
   </StrictMode>
 );
